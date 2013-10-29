695969fc7ac6211bba384ba55f7d8eeb52c514a8
Derek Flenniken <derek.flenniken@ucsf.edu>
2013-10-29 07:42:25 -0700

    scripts correct

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ config = {
     'version': '0.1',
     'install_requires': ['nose'],
     'packages': ['qualpy'],
-    'scripts': ['qualpy'],
+    'scripts': ['qualpy.py'],
     'name': 'qualpy'
 }
 
