9c7ac05a17246c1ef96d182207786a3a16c15f62
Derek Flenniken <derek@enth.net>
2014-02-01 09:44:20 -0800

    default sphinx template

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,6 @@
 import sys
 import os
 
-import sphinx_rtd_theme
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -104,7 +102,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
